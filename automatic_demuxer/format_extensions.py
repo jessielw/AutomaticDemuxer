075bfcb9e3bf6b652f86bc9f7dec6d75ea434eed
jlw4049 <jlw_4049@hotmail.com>
2022-09-09 22:10:11 -0400

    Add video_format dictionary

---
+++ automatic_demuxer/format_extensions.py
@@ -0,0 +1,15 @@
+video_formats = {
+    "V_MS/VFW/FOURCC": "avi",
+    "V_UNCOMPRESSED": "raw",
+    "V_MPEG4/ISO/AVC": "avc",
+    "V_MPEGH/ISO/HEVC": "hevc",
+    "V_MPEG4/MS/V3": "mp4",
+    "V_MPEG1": "mpg",
+    "V_MPEG2": "mpg",
+    "V_REAL/": "rm",
+    "V_QUICKTIME": "mov",
+    "V_THEORA": "ogv",
+    "V_PRORES": "mov",
+    "V_VP": "ivf",
+    "V_DIRAC": "drc",
+}
