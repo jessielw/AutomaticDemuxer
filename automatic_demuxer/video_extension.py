ad2475277b66ffdccd9185ad743e836f24486522
jlw4049 <jlw_4049@hotmail.com>
2022-09-16 14:54:06 -0400

    Update video_extension.py

--- automatic_demuxer/video_extension.py
+++ automatic_demuxer/video_extension.py
@@ -3,32 +3,36 @@ from __future__ import annotations
 import pathlib
 from os import PathLike
 
-from pymediainfo import MediaInfo
-
-from format_extensions import video_formats
+from automatic_demuxer.format_extensions import mkv_video_formats_conversion
 
 
-def get_video_extension(file_input: str | PathLike, extension: str | None):
+def get_video_extension(
+    file_input: str | PathLike,
+    media_info_instance: object,
+    track_number: str | int,
+    fallback_ext: str,
+    extension: str | None,
+):
     """
     Check for video tracks and automatically generate the elementary video with a proper extension to demux
 
     :param file_input: String or Pathlike to video input
+    :param media_info_instance: Object of a mediainfo class instance
+    :param track_number: Integer to be sent to mediainfo to select the video track. Default is 0.
+    :param fallback_ext: String in the form of an extension, default is "mkv" but can be overridden.
     :param extension: If set to None we will automatically determine the output extension based off of the codec
     ID. If an extension is supplied automatic detection will be skipped.
     :return: A string in the format of ".avc"
     """
 
-    # open the input file with mediainfo
-    parse_file = MediaInfo.parse(pathlib.Path(file_input))
-
     # if there is no video tracks
-    if not parse_file.video_tracks:
+    if not media_info_instance.video_tracks:
         raise Exception(
             f"{str(pathlib.Path(file_input).name)} does not contain any video tracks"
         )
 
     # if there is a video track
-    elif parse_file.video_tracks:
+    elif media_info_instance.video_tracks:
         # if extension was provided clean the input up and return it
         if extension:
             return str(f".{extension.lower().replace('.', '')}")
@@ -37,16 +41,24 @@ def get_video_extension(file_input: str | PathLike, extension: str | None):
         # extension list
         elif not extension:
 
-            # if extension is found return it
-            try:
-                generate_extension = video_formats[
-                    str(parse_file.video_tracks[0].codec_id).upper()
-                ]
-                return str(f".{generate_extension}")
+            # if file is mkv or webm attempt to automatically detect video output extension based off codec id
+            if (
+                pathlib.Path(file_input).suffix == "mkv"
+                or pathlib.Path(file_input).suffix == "webm"
+            ):
+                # if extension is found return it
+                try:
+                    generate_extension = mkv_video_formats_conversion[
+                        str(
+                            media_info_instance.video_tracks[int(track_number)].codec_id
+                        ).upper()
+                    ]
+                    return str(f".{generate_extension}")
 
-            # if codec ID does not match any in the dictionary raise an error
-            except KeyError:
-                raise KeyError(
-                    f"Could not automatically detect video output extension for codec "
-                    f"'{str(parse_file.video_tracks[0].codec_id).upper()}'"
-                )
+                # if codec ID does not match any in the dictionary return fallback_ext
+                except KeyError:
+                    return str(f".{fallback_ext}")
+
+            # if file is anything other than mkv/webm default to the fallback_ext
+            else:
+                return str(f".{fallback_ext}")
