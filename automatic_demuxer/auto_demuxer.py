cfc09610894a7a4750de6526d30f50535a56b603
jlw4049 <jlw_4049@hotmail.com>
2022-09-16 16:34:54 -0400

    Add support for delay and output suffix control

--- automatic_demuxer/auto_demuxer.py
+++ automatic_demuxer/auto_demuxer.py
@@ -12,11 +12,11 @@ from pymediainfo import MediaInfo
 
 
 class AutoDemuxer:
-
     def __init__(self):
         """set default variables to be utilized"""
         self.parse_file = None
         self.duration = None
+        self.delay = None
         self.file = None
         self.ffmpeg = None
         self.track_number = None
@@ -64,13 +64,17 @@ class AutoDemuxer:
             elif not ffmpeg_on_path:
                 raise Exception("Could not locate ffmpeg")
 
-    def video_demux(self,
-                    file_input: str | PathLike,
-                    ffmpeg_path: str | PathLike | None = None,
-                    track_number: int | str = 0,
-                    video_output_extension: str | None = None,
-                    callback: Optional[Callable[[dict], None]] | None = None,
-                    fallback_ext: str = "mkv"):
+    def video_demux(
+        self,
+        file_input: str | PathLike,
+        ffmpeg_path: str | PathLike | None = None,
+        track_number: int | str = 0,
+        suffix: str = "_out_",
+        insert_delay: bool = True,
+        video_output_extension: str | None = None,
+        callback: Optional[Callable[[dict], None]] | None = None,
+        fallback_ext: str = "mkv",
+    ):
         """
         Take all the input parameters and utilize them to extract the video from the input file.
         The only required parameter is file_input.
@@ -78,6 +82,8 @@ class AutoDemuxer:
         :param file_input: String or Pathlike path to input file.
         :param ffmpeg_path: String or Pathlike path to ffmpeg.
         :param track_number: Track number based off of ffmpeg stream selection or mediainfo's 'Stream identifier'.
+        :param suffix: String to define the output ending suffix, default is _out_.
+        :param insert_delay: Bool to insert delay string into filename output if delay is detected.
         :param video_output_extension: The output extension in the form of a string. e.g. "mkv" or ".mkv"
         :param callback: Log callback progress to a variable instead of printing to consoleStream identifier
         :param fallback_ext: Extension on the form of a string. e.g. "mkv" or "mp4". Default is "mkv", if changed the
@@ -89,31 +95,60 @@ class AutoDemuxer:
         self.parse_file = MediaInfo.parse(pathlib.Path(file_input))
         self.duration = self.parse_file.video_tracks[int(track_number)].duration
 
+        # detect video delay
+        if insert_delay:
+            self.delay = self.parse_file.video_tracks[int(track_number)].delay
+            if self.delay:
+                self.delay = f"[delay {str(self.delay)}ms]"
+            elif not self.delay:
+                self.delay = ""
+        elif not insert_delay:
+            self.delay = ""
+
         self.file = self._check_file(file_input)
         self.ffmpeg = self._check_ffmpeg(ffmpeg_path)
         self.track_number = track_number
-        self.extension = get_video_extension(file_input, self.parse_file, track_number, fallback_ext,
-                                             video_output_extension)
+        self.extension = get_video_extension(
+            file_input,
+            self.parse_file,
+            track_number,
+            fallback_ext,
+            video_output_extension,
+        )
         self.callback = callback
 
         # create completed output directory
         output_dir = pathlib.Path(self.file).parent
-        output_file_name = pathlib.Path(str(pathlib.Path(pathlib.Path(self.file).name).with_suffix("")) +
-                                        str(self.extension))
+        output_file_name = pathlib.Path(
+            str(pathlib.Path(pathlib.Path(self.file).name).with_suffix(""))
+            + str(self.delay)
+            + suffix
+            + str(self.extension)
+        )
         output = pathlib.Path(output_dir / output_file_name)
 
         # build out command
-        command = [self.ffmpeg, "-y", "-i", self.file, "-map", f"0:v:{str(self.track_number)}", "-c:v:0", "copy",
-                   output, "-hide_banner"]
+        command = [
+            self.ffmpeg,
+            "-y",
+            "-i",
+            self.file,
+            "-map",
+            f"0:v:{str(self.track_number)}",
+            "-c:v:0",
+            "copy",
+            output,
+            "-hide_banner",
+        ]
 
         # run the demuxer class to demux the file
         Demuxer(command, self.duration, callback=self.callback)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # example of video_demux with a call back "testing"
     def testing(x):
         print(x)
 
     demux = AutoDemuxer()
-    demux.video_demux(file_input=r"fileinput.mkv", callback=testing)
+    demux.video_demux(file_input=r"file_input.mkv", callback=testing)
