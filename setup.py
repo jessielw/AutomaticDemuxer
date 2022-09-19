f6eee8e2f159ffccd0d621a2714a57a2c6f205f3
jlw4049 <jlw_4049@hotmail.com>
2022-09-19 11:54:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,16 +15,12 @@ classifiers = [
 with open(os.path.join(os.path.dirname(__file__), "README.md")) as fd:
     ext_long_desc = fd.read()
 
-long_desc = (
-    "A package that utilizes ffmpeg to automatically demux tracks into single file formats. "
-    "See https://github.com/jlw4049/AutomaticDemuxer for more information" + "\n\n\n" + ext_long_desc
-)
-
 setup(
     name="AutomaticDemuxer",
     version="1.34",
     description="Automatically Demux tracks from media-files",
-    long_description=long_desc,
+    long_description=ext_long_desc,
+    long_description_content_type="text/markdown",
     url="https://github.com/jlw4049/AutomaticDemuxer",
     author="Jessie Wilson",
     author_email="jessielw4049@gmail.com",
